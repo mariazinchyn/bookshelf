314a3da528eddb324728968b13baccb9ff8f2af5
mariazinchyn <mariazinchyn@gmail.com>
2020-04-10 18:45:20 +0300

    first controller and html

--- src/main/java/com/maria/bookshelf/controller/BookController.java
+++ src/main/java/com/maria/bookshelf/controller/BookController.java
@@ -1,8 +1,14 @@
 package com.maria.bookshelf.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 
 public class BookController {
+    @GetMapping("/")
+    public String blogMain(Model model){
+        return "main";
+    }
 }
